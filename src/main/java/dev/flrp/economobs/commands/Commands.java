176287a632ad021bbec1729ffd445d7a57108a35
flrp <34534419+flrping@users.noreply.github.com>
2021-04-01 20:23:05 -0400

    1.1.5
    Fixed NoClassFound error for legacy versions when using WildStacker as your stacker type.

--- src/main/java/dev/flrp/economobs/commands/Commands.java
+++ src/main/java/dev/flrp/economobs/commands/Commands.java
@@ -19,7 +19,7 @@ public class Commands extends CommandBase {
 
     @Default
     public void defaultCommand(final CommandSender commandSender) {
-        commandSender.sendMessage(plugin.getLocale().parse("&a&lEconomobs &7Version 1.1.4 &8| &7By flrp <3"));
+        commandSender.sendMessage(plugin.getLocale().parse("&a&lEconomobs &7Version 1.1.5 &8| &7By flrp <3"));
         commandSender.sendMessage(plugin.getLocale().parse("&f/economobs help &8- &7Displays this menu."));
         if(commandSender.hasPermission("economobs.admin")) {
             commandSender.sendMessage(plugin.getLocale().parse("&f/economobs reload &8- &7Reloads the plugin."));
--- src/main/java/dev/flrp/economobs/listeners/WildStackerListener.java
+++ src/main/java/dev/flrp/economobs/listeners/WildStackerListener.java
@@ -3,13 +3,12 @@ package dev.flrp.economobs.listeners;
 import com.bgsoftware.wildstacker.api.events.EntityUnstackEvent;
 import dev.flrp.economobs.Economobs;
 import dev.flrp.economobs.configuration.StackerType;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-
-import java.util.UUID;
 
 public class WildStackerListener implements Listener {
 
@@ -26,14 +25,14 @@ public class WildStackerListener implements Listener {
         if(event.getUnstackSource() == null) return;
         if(plugin.getMythicMobs() != null && plugin.getMythicMobs().getMobManager().getActiveMob(event.getEntity().getUniqueId()).isPresent()) return;
 
-        UUID id = event.getUnstackSource().getUniqueId();
+        Entity source = event.getUnstackSource();
         LivingEntity entity = event.getEntity().getLivingEntity();
         // Entity Checks
-        if(plugin.getServer().getEntity(id).getType() != EntityType.PLAYER) return;
+        if(source.getType() != EntityType.PLAYER) return;
         if(plugin.getConfig().getStringList("world-blacklist").contains(entity.getWorld().getName())) return;
         if(!plugin.getMobManager().getAmounts().containsKey(entity.getType())) return;
 
-        Player player = (Player) plugin.getServer().getEntity(id);
+        Player player = plugin.getServer().getPlayer(source.getUniqueId());
         plugin.getEconomyManager().handleDeposit(player, entity, plugin.getMobManager().getAmount(entity.getType()), event.getAmount());
     }
 
